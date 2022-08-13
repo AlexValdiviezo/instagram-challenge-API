b7f657f230de32b986145a60bcada8846a37900b
Alex Valdiviezo <alex99valdiviezo@gmail.com>
2022-08-13 13:52:22 -0300

    upload files added

--- controllers/upload.js
+++ controllers/upload.js
@@ -1,13 +1,22 @@
 const { request } = require('express');
 const { response } = require('express');
 const PublicItem = require('../models/public-items');
+const cloudinary = require('../utils/cloudinary')
 
 const uploadPost = async (request, response) => {
-    const {destination, filename} = request.file;
-    response.status(202).json({
-        msg: 'ok',
-        url: destination + filename
-    });
+   try {
+      const {url} = await cloudinary.uploader.upload(request.file.path, {
+         folder: 'instagram-challenge'
+      });
+      response.status(202).json({
+         url
+      })
+   } catch (error) {
+      response.status(402).json({
+         msg: 'bad gateway',
+         error
+        })
+   }
 }
 
 module.exports = {uploadPost}
--- routes/upload.js
+++ routes/upload.js
@@ -1,20 +1,15 @@
 const { Router, request , response, next} = require('express');
 const router = Router();
-const {uploadPost} = require('../controllers/upload');
 const multer = require('multer');
 const mimeTypes = require('mime-types')
 
-const storage = multer.diskStorage({
-    destination: __dirname + '/uploads/',
-    filename: function(req, file, cb){
-        cb("", Date.now() + file.originalname +"." + mimeTypes.extension(file.mimetype));
-    }
-})
+const {
+    uploadPost
+} = require('../controllers/upload');
 
-const upload = multer({
-    storage: storage
-})
+const upload = require('../utils/multer');
 
-router.post('/', [upload.single('photo')], uploadPost);
+
+router.post('/', upload.single('photo'), uploadPost);
 
 module.exports = router;
